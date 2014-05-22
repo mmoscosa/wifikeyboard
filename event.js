07f2802a37554ec5457a2cc46d81939e8d198871
Martin Moscosa <mmoscosa@gmail.com>
2014-05-22 12:08:47 -0500

    cleaning up the code

--- event.js
+++ event.js
@@ -1,31 +1,47 @@
 $(function() {
-    var placeholder, submit, url, urlCookie;
+    var placeholder, submit, url;
     placeholder 	=	$('#placeholder');
     submit			=	$('#submit');
     url 			=	$('#url');
-    urlCookie 		= 	monster.get('wifiKeyboardURL');
 	
 
-	function loadWifiKeyboard (formAction, urlAddress) {
-		if(urlCookie !== null){
-			if(formAction == 'submit'){
-				monster.set('wifiKeyboardURL', urlAddress, 1);
-			}else{
-				url.val(monster.get('wifiKeyboardURL'));
-				submit.attr('value', 'Change');
-				placeholder.replaceWith('<div style="border: 1px solid red; width: 400px; height: 325px; overflow: hidden;"><iframe scrolling="no" style="width: 800px; height: 600px; margin-top: -250px; margin-left: -20px;" src="'+monster.get('wifiKeyboardURL')+'" width="800" height="600" border="0"></iframe></div>');
-			}
-		}else{
-			if(formAction == 'submit'){
-				monster.set('wifiKeyboardURL', urlAddress, 1);
-			}else{
-				return false;
-			}
-		}	
+    //Check if URL Exists
+    // @Params: URL to check
+    // @Return if exists then set cookie and reload page
+    // @Return if it DOES NOT exists then delete cookie and reload page
+    function urlExists(url, callback){
+	  $.ajax({
+	    type: 'HEAD',
+	    url: url,
+	    success: function(){
+	    	monster.set('wifiKeyboardURL', url, '1');
+	    	location.reload();
+	    },
+	    error: function() {
+	    	monster.remove('wifiKeyboardURL');
+	    	location.reload();
+	    }
+	  });
 	}
-	loadWifiKeyboard(null, null);
-    submit.click(function(){
-    	loadWifiKeyboard('submit', url.val());
+
+	function loadWifiKeyboard () {
+		if(monster.get('wifiKeyboardURL')){
+			//ToDO Check if URL in cookie is still valid 
+			
+			//else load iframe
+			url.val(monster.get('wifiKeyboardURL'));
+			submit.attr('value', 'Change');
+	    	placeholder.replaceWith('<div style="border: 1px solid red; width: 400px; height: 325px; overflow: hidden;"><iframe scrolling="no" style="width: 800px; height: 600px; margin-top: -250px; margin-left: -20px;" src="'+monster.get('wifiKeyboardURL')+'" width="800" height="600" border="0"></iframe></div>');
+		}else{
+			placeholder.replaceWith('error');
+		}
+	}
+	
+	loadWifiKeyboard();
+    
+    //When button is pressed, check if url exist
+    $('#submit').click(function() {
+    	urlExists(url.val());
     	return false;
     });
 });
